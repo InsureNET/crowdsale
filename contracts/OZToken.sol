9e2430609e942d27060bf93bd90aaf4236987379
Jaxcoder <codenamejason@outlook.com>
2020-03-02 14:52:54 -0500

    working deployed local token

--- contracts/OZToken.sol
+++ contracts/OZToken.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
